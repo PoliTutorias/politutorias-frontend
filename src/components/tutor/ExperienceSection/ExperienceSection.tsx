0d30a3b592fbf7b4c3f6c40619bd4a9f085af192
hoc1ey <jose.merchan01@epn.edu.ec>
2026-03-09 22:17:48 -0500

    HU32 style: cambiar línea izquierda experiencia a color amarillo (--yellow)

--- src/components/tutor/ExperienceSection/ExperienceSection.tsx
+++ src/components/tutor/ExperienceSection/ExperienceSection.tsx
@@ -15,7 +15,7 @@ export default function ExperienceSection({ experiences }: ExperienceSectionProp
 
       <div className="space-y-3">
         {experiences.map((exp, index) => (
-          <div key={index} className="border-l-4 border-yellow pl-4 pb-3 last:pb-0">
+          <div key={index} className="border-l-4 border-[color:var(--yellow)] pl-4 pb-3 last:pb-0">
             <h4 className="text-xs font-bold text-foreground">{exp.position}</h4>
             <p className="text-xs text-text-secondary">{exp.institution}</p>
             <p className="text-xs text-text-secondary">{exp.period}</p>
